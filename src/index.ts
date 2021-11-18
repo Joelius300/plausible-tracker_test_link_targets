3feb9eadf05fc73823f657bc74de25e0dca07a2f
Joel Liechti <bstlng321@gmail.com>
2021-11-18 16:02:40 +0100

    Include plausible-tracker from fork
    Also I changed the links to different paths within example.org so it's visible on the plausible dashboard. I've not setup gh pages but when using trackLocalhost, it's not necessary to do so anyway.

--- src/index.ts
+++ src/index.ts
@@ -1,1 +1,11 @@
+import Plausible from "plausible-tracker";
+
 console.log("we're up and running");
+
+const plausible = Plausible({
+  domain: 'joelius300.github.io/plausible-tracker_test-link-targets',
+  trackLocalhost: true,
+});
+
+plausible.trackPageview();
+plausible.enableAutoOutboundTracking();
